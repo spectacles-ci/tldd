b7950c3d4425806354400cea19c13d6effc35d07
Dylan Baker <dylan@spectacles.dev>
2024-06-25 16:26:38 +0100

    more endpoints

--- src/vertex_dashboards/main.py
+++ src/vertex_dashboards/main.py
@@ -14,7 +14,13 @@ from google.cloud.logging import Client as LoggingClient
 from google.cloud.storage import Client as StorageClient
 from vertexai.generative_models import GenerativeModel, Part
 
-from vertex_dashboards.models import DashboardWebhook, Summarizer, Summary
+from vertex_dashboards.models import (
+    DashboardWebhook,
+    Receipt,
+    Summarizer,
+    Summary,
+    SummaryRequest,
+)
 
 PROJECT_ID = "vertex-dashboards"
 
@@ -66,6 +72,55 @@ async def delete_summarizer(summarizer_id: str) -> None:
     firestore_client.collection("summarizers").document(summarizer_id).delete()
 
 
+@app.post("/summarizer/{summarizer_id}/summarise")
+async def summarise(data: SummaryRequest) -> dict[str, Any]:
+    """Endpoint to run a summarizer."""
+    summarizer = data.summarizer  # noqa: F841
+    receipt = data.receipt
+
+    # Run the summarizer
+    vertexai.init(project=PROJECT_ID, location="us-central1")
+    model = GenerativeModel(model_name="gemini-1.5-pro-001")
+    # TODO: Prompt engineering and RAG
+    prompt = """
+        You are a very professional document summarization specialist.
+        Please summarize the given document.
+    """
+    report_location = f"gs://vertex-dashboards/{receipt.report_location}"
+    pdf_file = Part.from_uri(report_location, mime_type="application/pdf")
+    contents = [pdf_file, prompt]
+    response = model.generate_content(contents)
+
+    # Save the Summary
+    summary = Summary(
+        body=response.text,
+        prompt=prompt,
+        report_location=receipt.report_location,
+        recipients=summarizer.recipients,
+        summarizer_id=summarizer.id,
+        timestamp=datetime.now(),
+    )
+    timestamp = receipt.timestamp.strftime("%Y%m%d%H%M%S")
+    firestore_client.collection("summaries").document(
+        f"{summarizer.id}-{timestamp}"
+    ).set(summary.model_dump())
+
+    return summary.model_dump()
+
+
+@app.get("/summarizer/{summarizer_id}/receipt")
+async def get_last_receipt(summarizer_id: str) -> dict[str, Any]:
+    """Endpoint to get the last receipt."""
+    receipt_doc = (
+        firestore_client.collection("receipts")
+        .where("summarizer_id", "==", summarizer_id)
+        .order_by("timestamp", direction="DESCENDING")
+        .limit(1)
+        .get()
+    )
+    return receipt_doc[0].to_dict()  # type: ignore[no-any-return]
+
+
 @app.post("/webhook/{summarizer_id}")
 async def run_summarizer(summarizer_id: str, webhook: DashboardWebhook) -> None:
     """Endpoint to run a summarizer.
@@ -80,7 +135,6 @@ async def run_summarizer(summarizer_id: str, webhook: DashboardWebhook) -> None:
         firestore_client.collection("summarizers").document(summarizer_id).get()
     )
     summarizer_dict = summarizer.to_dict()
-    summarizer_config = Summarizer(**summarizer_dict)
 
     # Decode the attachment
     attachment_data = webhook.attachment.data
@@ -94,45 +148,26 @@ async def run_summarizer(summarizer_id: str, webhook: DashboardWebhook) -> None:
     blob.upload_from_string(decoded_data, content_type="application/pdf")
     logger.info(f"Decoded data has been uploaded to GCS bucket: {blob.name}")
 
-    # Run the summarizer
-    vertexai.init(project=PROJECT_ID, location="us-central1")
-    model = GenerativeModel(model_name="gemini-1.5-pro-001")
-    pdf_file_uri = f"gs://{bucket.name}/{blob.name}"
-    # TODO: Prompt engineering and RAG
-    prompt = """
-You are provided with a dashboard containing various data visualizations, charts, graphs, and key metrics. Your task is to analyze the dashboard and provide a comprehensive, data-dense summary. Follow these guidelines to ensure a thorough and accurate analysis:
+    # Save the receipt to Firestore
+    receipt = Receipt(
+        timestamp=datetime.now(),
+        report_location=blob.name,
+        summarizer_id=summarizer_id,
+    )
+    firestore_client.collection("receipts").document(
+        f"{summarizer_id}-{timestamp}"
+    ).set(receipt.model_dump())
 
-	1.	Overall Summary: Start with a brief overview of the dashboard’s theme and purpose.
-	2.	Key Metrics and Trends: Identify and describe the most important metrics and trends. Highlight significant changes, specific values, and relevant timeframes.
-	3.	Interesting Insights: Focus on notable data points and insights. Emphasize unusual patterns, correlations, or significant changes, ensuring accuracy.
-	4.	Comparative Analysis: Summarize any comparisons (e.g., year-over-year, month-over-month, category comparisons) and what they indicate.
-	5.	Performance Indicators: Discuss key performance indicators (KPIs) and their status. Mention if they are meeting, exceeding, or falling short of targets.
-	6.	Actionable Insights: Identify actionable insights derived from the data. Suggest possible actions or decisions informed by these insights.
-	7.	Confidence Level: Only highlight data and insights that you are confident in. Avoid unclear or ambiguous points.
+    # if summarizer doesn't exist, don't process
+    if not summarizer_dict:
+        logger.info(f"Summarizer {summarizer_id} does not exist.")
+        return
 
-Example Analysis Structure:
+    summarizer_config = Summarizer(**summarizer_dict)
 
-	•	Introduction: Briefly introduce the dashboard’s purpose and focus.
-	•	Key Findings: List significant metrics and trends with specific details.
-	•	Detailed Insights: Elaborate on noteworthy and confident data points.
-	•	Comparative Analysis: Discuss any relevant comparative data.
-	•	Conclusion: Summarize actionable insights and suggest potential actions.
-
-Sample Text:
-
-“The dashboard provides an overview of the company’s sales performance over the past year. Key metrics include a 15% increase in monthly sales with a peak in December 2023 and a steady upward trend in quarterly revenue.
-
-A notable insight is the correlation between marketing spend and sales performance, particularly in North America and Europe, which have higher sales figures linked to increased marketing investments.
-
-Comparative analysis shows that Q4 2023 sales were 20% higher than Q4 2022, indicating effective holiday sales strategies.
-
-The data suggests increasing marketing efforts in underperforming regions could boost sales. Maintaining current strategies in high-performing regions is advisable to sustain growth.
-
-Overall, the dashboard highlights a positive sales trajectory with actionable insights for further growth.
-    """
-    pdf_file = Part.from_uri(pdf_file_uri, mime_type="application/pdf")
-    contents = [pdf_file, prompt]
-    response = model.generate_content(contents)
+    response = await summarise(
+        SummaryRequest(summarizer=summarizer_config, receipt=receipt)
+    )
 
     resend.api_key = os.getenv("RESEND_API_KEY")
     attachment = resend.Attachment(filename="dashboard.pdf", content=attachment_data)
@@ -141,23 +176,10 @@ Overall, the dashboard highlights a positive sales trajectory with actionable in
             "from": "hello@spectacles.dev",
             "to": summarizer_config.recipients,
             "subject": "Your Dashboard Has Been AI Analyzed!",
-            "html": f"<p>Here is the summary of your dashboard: {response.text}</p>",
+            "html": f"<p>Here is the summary of your dashboard: {response['body']}</p>",
             "attachments": [attachment],
         }
     )
-
-    # Save the Summary
-    summary = Summary(
-        body=response.text,
-        prompt=prompt,
-        report_location=pdf_file_uri,
-        recipients=summarizer_config.recipients,
-        summarizer_id=summarizer_id,
-        timestamp=datetime.now(),
-    )
-    firestore_client.collection("summaries").document(
-        f"{summarizer_id}-{timestamp}"
-    ).set(summary.model_dump())
 
 
 def main() -> None:
