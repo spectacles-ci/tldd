b46e5bde4beb0dab335b72bf2d6afa74a4f31e66
Josh Temple <8672171+joshtemple@users.noreply.github.com>
2024-06-25 10:34:12 -0500

    Save progress with React Query

--- frontend/src/HelloWorld.tsx
+++ frontend/src/HelloWorld.tsx
@@ -1,9 +1,29 @@
 import React, { useContext } from "react";
 import { ExtensionContext } from "@looker/extension-sdk-react";
-import { Button, Input, SummarizerTable } from "./components";
+import { Button, SummarizerTable } from "./components";
+import { useQuery } from "@tanstack/react-query";
 
 export const HelloWorld: React.FC = () => {
   const { core40SDK } = useContext(ExtensionContext);
+  const query = useQuery({
+    queryKey: ["summarizers"],
+    queryFn: () => {
+      return [
+        {
+          id: "1",
+          name: "Marketing Weekly",
+          lastReceived: "2024-06-24T12:00:00Z",
+          recipients: ["sherry@petgourmet.com", "john@doe.com", "jane@doe.com"],
+        },
+        {
+          id: "2",
+          name: "Financial Health QBR",
+          lastReceived: "2024-03-07T12:00:00Z",
+          recipients: ["juan.mendoza@petgourmet.com", "john@doe.com"],
+        },
+      ];
+    },
+  });
 
   return (
     <div className="container mt-8 mx-auto px-4">
@@ -11,26 +31,7 @@ export const HelloWorld: React.FC = () => {
         <h1 className="text-xl mb-4 text-gray-950">Summarizers</h1>
         <Button>Create Summarizer</Button>
       </div>
-      <SummarizerTable
-        summarizers={[
-          {
-            id: "1",
-            name: "Marketing Weekly",
-            lastReceived: "2024-06-24T12:00:00Z",
-            recipients: [
-              "sherry@petgourmet.com",
-              "john@doe.com",
-              "jane@doe.com",
-            ],
-          },
-          {
-            id: "2",
-            name: "Financial Health QBR",
-            lastReceived: "2024-03-07T12:00:00Z",
-            recipients: ["juan.mendoza@petgourmet.com", "john@doe.com"],
-          },
-        ]}
-      />
+      <SummarizerTable summarizers={query.data || []} />
     </div>
   );
 };
--- frontend/src/components/SummarizerTable.tsx
+++ frontend/src/components/SummarizerTable.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Button from "./Button";
 import type { SummarizerRow } from "../types";
 import { intlFormatDistance } from "date-fns";
 
@@ -13,7 +14,7 @@ export default function SummarizerTable({
   }
 
   return (
-    <table className="w-full rounded border-separate border-spacing-0 border border-gray-200 shadow">
+    <table className="w-full rounded border-separate border-spacing-0 border border-gray-200">
       <thead className="bg-gray-100 text-left text-gray-700 text-sm font-semibold border-b border-gray-200">
         <tr>
           <th className="px-6 py-3 w-full">Name</th>
@@ -54,6 +55,21 @@ export default function SummarizerTable({
           </tr>
         ))}
       </tbody>
+      <tfoot>
+        <tr>
+          <td colSpan={3} className="px-6 py-3 bg-gray-50">
+            <div className="flex justify-between items-center">
+              <span className="text-sm text-gray-700">
+                Showing 1 to 10 of 34 results
+              </span>
+              <div className="flex gap-x-4">
+                <Button variant="secondary">Previous</Button>
+                <Button variant="secondary">Next</Button>
+              </div>
+            </div>
+          </td>
+        </tr>
+      </tfoot>
     </table>
   );
 }
