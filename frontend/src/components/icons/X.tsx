70b11e7119fbccfe3d81e133599a63c4acc10008
Josh Temple <8672171+joshtemple@users.noreply.github.com>
2024-06-26 12:31:40 -0400

    Reformat with Prettier

--- frontend/src/components/icons/X.tsx
+++ frontend/src/components/icons/X.tsx
@@ -1,20 +1,16 @@
 import React from "react";
 
 export default function X({ className }: { className?: string }) {
-  return (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      fill="none"
-      viewBox="0 0 24 24"
-      strokeWidth="1.5"
-      stroke="currentColor"
-      className={className}
-    >
-      <path
-        strokeLinecap="round"
-        strokeLinejoin="round"
-        d="M6 18 18 6M6 6l12 12"
-      />
-    </svg>
-  );
+    return (
+        <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth="1.5"
+            stroke="currentColor"
+            className={className}
+        >
+            <path strokeLinecap="round" strokeLinejoin="round" d="M6 18 18 6M6 6l12 12" />
+        </svg>
+    );
 }
