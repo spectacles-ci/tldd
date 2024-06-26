aa8902ca6dbcc447bedde31b65585808ba977721
Tom <tom@spectacles.dev>
2024-06-26 17:47:07 +0100

    Fix

--- frontend/src/pages/CreateSummarizer.tsx
+++ frontend/src/pages/CreateSummarizer.tsx
@@ -7,12 +7,33 @@ import { SummarizerWebhook } from "../components/SummarizerWebhook";
 
 const API_URL = "https://vertex-dashboards-2w54ohrt4q-uc.a.run.app/";
 
+function generateShortUUID(): string {
+    const numberOfChars = 8;
+    const characters = "0123456789abcdefghijklmnopqrstuvwxyz";
+    const base = characters.length;
+    let number = Math.floor(Math.random() * Math.pow(base, numberOfChars)); // Generates a number between 0 and base^8 - 1
+    let shortUUID = "";
+
+    while (number > 0) {
+        shortUUID = characters[number % base] + shortUUID;
+        number = Math.floor(number / base);
+    }
+
+    // Ensure the length is consistent
+    while (shortUUID.length < numberOfChars) {
+        shortUUID = "0" + shortUUID;
+    }
+
+    return shortUUID;
+}
+
 export default function CreateSummarizer() {
+    const [webhookUrl, setWebhookUrl] = useState<string>("");
     const [isLoading, setLoading] = useState<boolean>(false);
 
     const form = useForm<Summarizer>({
         defaultValues: {
-            id: "",
+            id: generateShortUUID(),
             name: "",
             recipient: "",
             recipients: [],
@@ -22,7 +43,9 @@ export default function CreateSummarizer() {
         },
     });
 
-    const { setError } = form;
+    const { watch, setError } = form;
+
+    const summarizerId = watch("id");
 
     const onSubmit: SubmitHandler<Summarizer> = async (data) => {
         setLoading(true);
@@ -52,8 +75,8 @@ export default function CreateSummarizer() {
         <div className="container mt-8 mx-auto px-4 mb-8">
             <div className="max-w-3xl flex flex-col gap-y-6">
                 <h1 className="text-xl text-gray-950">Create a Summarizer</h1>
-                <SummarizerWebhook />
-                <SummarizerForm form={form} isLoading={isLoading} onSubmit={onSubmit} />
+                <SummarizerWebhook webhookUrl={webhookUrl} setWebhookUrl={setWebhookUrl} summarizerId={summarizerId} />
+                <SummarizerForm form={form} isLoading={isLoading} onSubmit={onSubmit} summarizerId={summarizerId} />
             </div>
         </div>
     );
