a0aaedf2496de50148c09d7f4f12fa819292b032
Tom <tom@spectacles.dev>
2024-06-26 17:47:07 +0100

    Fix

--- frontend/src/components/SummarizerWebhook.tsx
+++ frontend/src/components/SummarizerWebhook.tsx
@@ -1,27 +1,36 @@
-import React, { useContext, useEffect, useState } from "react";
+import React, { Dispatch, SetStateAction, useCallback, useContext, useEffect, useState } from "react";
 
 import { ExtensionContext } from "@looker/extension-sdk-react";
 import clsx from "clsx";
 
 import { Clipboard } from "./icons";
 
-export function SummarizerWebhook() {
-    const [webhookUrl, setWebhookUrl] = useState<string>("https://aed42f-summarize.run.app/a294cd29dfa322");
+export function SummarizerWebhook({
+    webhookUrl,
+    setWebhookUrl,
+    summarizerId,
+}: {
+    webhookUrl: string;
+    setWebhookUrl: Dispatch<SetStateAction<string>>;
+    summarizerId: string;
+}) {
     const [isCopied, setIsCopied] = useState<boolean>(false);
-    const [userAttribute, setUserAttribute] = useState<string>("");
 
-    const lookerExtension = useContext(ExtensionContext);
+    const extensionContext = useContext(ExtensionContext);
+    const { extensionSDK } = extensionContext;
+
+    const getAttribute = useCallback(async () => {
+        const attribute = await extensionSDK.userAttributeGetItem("tldd_api");
+        const apiUrl = attribute ?? "";
+        setWebhookUrl(`${apiUrl}/webhook/${summarizerId}`);
+    }, [summarizerId, extensionSDK]);
 
     useEffect(() => {
-        const getAttribute = async () => {
-            const attribute = await lookerExtension.extensionSDK.userAttributeGetItem("tldd_api");
-            setUserAttribute(attribute ?? "");
-        };
         getAttribute();
-    }, [lookerExtension]);
+    }, [getAttribute]);
 
     const copyToClipboard = async () => {
-        await lookerExtension.extensionSDK.clipboardWrite(webhookUrl);
+        await extensionSDK.clipboardWrite(webhookUrl);
         setIsCopied(true);
         setTimeout(() => setIsCopied(false), 2000);
     };
@@ -33,24 +42,26 @@ export function SummarizerWebhook() {
                 Create a schedule in Looker with the <span className="font-medium">Webhook</span> destination and
                 provide the URL below.
             </p>
-            <div className="flex items-center">
-                <button
-                    className={clsx(
-                        "bg-white border transition-colors duration-300 hover:bg-gray-50  text-gray-400 rounded shadow-sm text-sm px-2 py-1.5 flex items-center gap-x-1.5",
-                        {
-                            "border-primary": isCopied,
-                            "border-gray-200": !isCopied,
-                        }
-                    )}
-                    onClick={copyToClipboard}
-                >
-                    <span>{webhookUrl}</span>
-                    <Clipboard className="size-4 text-primary" />
-                </button>
-                <span className="ml-2 text-xs text-gray-400">
-                    {isCopied ? "Copied to clipboard!" : "Click to copy"}
-                </span>
-            </div>
+            {webhookUrl && (
+                <div className="flex items-center">
+                    <button
+                        className={clsx(
+                            "bg-white border transition-colors duration-300 hover:bg-gray-50  text-gray-400 rounded shadow-sm text-sm px-2 py-1.5 flex items-center gap-x-1.5",
+                            {
+                                "border-primary": isCopied,
+                                "border-gray-200": !isCopied,
+                            }
+                        )}
+                        onClick={copyToClipboard}
+                    >
+                        <span>{webhookUrl}</span>
+                        <Clipboard className="size-4 text-primary" />
+                    </button>
+                    <span className="ml-2 text-xs text-gray-400">
+                        {isCopied ? "Copied to clipboard!" : "Click to copy"}
+                    </span>
+                </div>
+            )}
         </div>
     );
 }
